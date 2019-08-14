ce6aa2027919c0d97e0a0437cee225de63b527ac
RodrigoFNascimento <rodrigofn0@hotmail.com>
2019-08-14 17:31:20 -0300

    Implement the Trie

--- rodrigonascimento_201600155174_autocompletar.java
+++ rodrigonascimento_201600155174_autocompletar.java
@@ -7,7 +7,132 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
+class Trie {
+
+    char letter;
+    boolean isWord;
+    Trie[] children;
+
+    public Trie() {
+
+        this.letter = ' ';
+        this.isWord = false;
+        this.children = new Trie[26];
+    }
+
+    public Trie(char letter) {
+
+        this.letter = letter;
+        this.isWord = false;
+        this.children = new Trie[26];
+    }
+}
+
 public class rodrigonascimento_201600155174_autocompletar {
+
+    public static Trie root = new Trie();
+
+    /**
+     * Inserts a key into a Trie.
+     * 
+     * @param key   Key to be inserted.
+     */
+    static void insert(String key) 
+    {
+        char currentChar;
+        Trie currentTrie = root;
+        int length = key.length(); 
+        int index;
+        
+        // Iterates through the letters in the word
+        for (int indexInThekey = 0; indexInThekey < length; indexInThekey++) 
+        {
+            currentChar = key.charAt(indexInThekey);
+            index = currentChar - 'a';
+            if (currentTrie.children[index] == null) 
+                currentTrie.children[index] = new Trie(currentChar);
+       
+            // Goes down the tree
+            currentTrie = currentTrie.children[index]; 
+        } 
+       
+        // Marks last node as word
+        currentTrie.isWord = true; 
+    } 
+
+    /**
+     * Searchs the Trie for compatible words.
+     * 
+     * @param word  Word to be searched.
+     * @return      Every word found.
+     */
+    static StringBuilder search(String word) {
+
+        int charIndex;
+        Trie currentTrie = root;
+        char currentChar;
+        StringBuilder foundWords = new StringBuilder();
+        StringBuilder prefix = new StringBuilder();
+        int wordLength = word.length();
+
+        for (int i = 0; i < wordLength; i++) {
+
+            currentChar = word.charAt(i);
+            charIndex = currentChar - 'a';
+
+            if (currentTrie.children[charIndex] != null) {
+
+                prefix.append(currentChar);
+
+                if (currentTrie.children[charIndex].isWord)
+                    foundWords.append(prefix + ",");
+
+                currentTrie = currentTrie.children[charIndex];
+            } else {
+                break;
+            }
+        }
+
+        if (prefix.length() > 0)
+            foundWords.append(getAllWords(currentTrie, new StringBuilder(), prefix));
+
+        if (foundWords.length() == 0)
+            foundWords.append('-');
+        else
+            foundWords.deleteCharAt(foundWords.length() - 1);   // There will be a ',' at the end
+
+        return foundWords;
+    }
+
+    /**
+     * Gets every word in the root's children.
+     * 
+     * @param root      Root of the Trie.
+     * @param output    The method's output.
+     * @param prefix    The Trie's prefix.
+     * @return          Every word found in the Trie.
+     */
+    public static StringBuilder getAllWords(Trie root, StringBuilder output, StringBuilder prefix) {
+
+        if (root == null)
+            return new StringBuilder();
+
+        for (int i = 0; i < root.children.length; i++) {
+
+            if (root.children[i] != null) {
+
+                prefix.append(root.children[i].letter);
+
+                if (root.children[i].isWord)
+                    output.append(prefix.toString() + ",");
+
+                getAllWords(root.children[i], output, prefix);
+                prefix.deleteCharAt(prefix.length() - 1);
+            }
+        }
+
+        return output;
+    }
 
     /**
      * Writes content to file.
@@ -32,6 +157,28 @@ public class rodrigonascimento_201600155174_autocompletar {
         try (FileInputStream inputStream = new FileInputStream(args[0])) {
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            // Inserts keys into Trie
+            int numberOfInsertions = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < numberOfInsertions; i++) {
+                insert(reader.readLine());
+            }
+
+            // Retrieves words from Trie
+            String searchedWord;
+            StringBuilder foundWords = new StringBuilder();
+            int numberOfSearches = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < numberOfSearches; i++) {
+
+                foundWords = new StringBuilder();
+
+                searchedWord = reader.readLine();
+
+                foundWords = search(searchedWord);
+
+                writeToFile(args[1], searchedWord + ":" + foundWords + "\n");
+                
+            }
 
         } catch (Exception ex) {
             ex.printStackTrace();
